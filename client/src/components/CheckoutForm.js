4b945cd0d503b2a208d1e95b474611c8013d3e2e
Blayze <blayzestone@protonmail.com>
2020-05-08 09:32:52 -0700

    Setup tests for CheckoutForm component
    Tests:
    * Form header renders
    * Form renders success message when the user submits
    * details entered by user are rendered inside of success message
    Both tests pass. Tests fail when intentionally broken.

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -63,7 +63,7 @@ const CheckoutForm = (props) => {
           Zip:
           <input name="zip" value={values.zip} onChange={handleChanges} />
         </label>
-        <button>Checkout</button>
+        <button data-testid="checkoutBtn">Checkout</button>
       </form>
 
       {showSuccessMessage && (
